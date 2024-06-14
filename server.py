8c7e4360687bcceb61e5ea6dc1b2a87acb50e1bc
KubaLisowski <kubalisowski94@gmail.com>
2024-06-14 18:26:33 +0200

    client into flask

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__, template_folder="../client/template", static_folder="../client")
+app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret!"
 socketio = SocketIO(app)
 
