f4db6575393864bdcdf6131567db2c9d484b04dc
KubaLisowski <kubalisowski94@gmail.com>
2024-10-24 21:26:15 +0200

    sql

--- database/sql/create_tables.sql
+++ database/sql/create_tables.sql
@@ -1,5 +1,29 @@
-create or replace table test (
-	id INT NOT NULL AUTO_INCREMENT,
-    txt VARCHAR(100) NULL,
-	PRIMARY KEY (id)
-);
+CREATE TABLE `game_object` (
+  `id` 		int(11)  	 NOT NULL AUTO_INCREMENT,
+  `x` 		int(11)  	 DEFAULT NULL,
+  `y` 		int(11)  	 DEFAULT NULL,
+  `width`   int(11)  	 DEFAULT NULL,
+  `height`  int(11)  	 DEFAULT NULL,
+  `frameX`  int(11) 	 DEFAULT NULL,
+  `frameY`  int(11) 	 DEFAULT NULL,
+  `speed`   int(11) 	 DEFAULT NULL,
+  `moving`  bit(1)	 	 DEFAULT NULL,
+  `img_src` varchar(100) DEFAULT NULL,
+  `player` 	bit(1)	 	 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
+
+CREATE TABLE `map` (
+  `id` 	   int(11) 		NOT NULL AUTO_INCREMENT,
+  `name`   varchar(100) DEFAULT NULL,
+  `width`  int(11) 		DEFAULT NULL,
+  `height` int(11) 		DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE map_object (
+  `id` 	   		   int(11) NOT NULL AUTO_INCREMENT,
+  `map_id` 		   int(11) NOT NULL,
+  `game_object_id` int(11) NOT null,  
+  PRIMARY KEY (`id`)
+)
